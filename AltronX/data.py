c795549d431710c2fddc63fea5e916bb2ee4a60f
badmunda98 <152181482+badmunda98@users.noreply.github.com>
2024-01-17 12:38:37 +0530

    Update data.py

--- AltronX/data.py
+++ AltronX/data.py
@@ -308,7 +308,7 @@ REPLYRAID = [
     "TERI SEXY BAHEN KI CHUT OP",
 ]
 
-GROUP = [-1001868408316]
+GROUP = [-1002056907061]
 
 PORMS = [
         "https://te.legra.ph/file/a66008b78909b431fc92b.mp4",
